ae252cc9f1258254214aef8e2b305627418b42d6
PeiroJack <3532926353@qq.com>
2020-03-12 08:05:40 +0800

    add sout Test...

--- src/com/imooc/servlet/GitTest2.java
+++ src/com/imooc/servlet/GitTest2.java
@@ -1,10 +1,10 @@
 package com.imooc.servlet;
 
-public class GitTest {
+public class GitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test...");
+		System.out.println("Test2...");
 	}
 
 }
